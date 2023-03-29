4deef52fabf3df0580eaa5967a5a0295b251ad99
David Boyer <daboyer@c3r5p10.42perpignan.fr>
2023-03-29 11:29:24 +0200

    Initalize repo

---
+++ minitalk.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daboyer <daboyer@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/29 11:24:38 by daboyer           #+#    #+#             */
+/*   Updated: 2023/03/29 11:25:57 by daboyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <stdio.h>
+#endif
